98a9a39ba8c435297cb6378c7b440f4d100ae453
mbeka02 <antonymbeka@gmail.com>
2025-03-01 23:36:28 +0300

    fix: added default value for the day_of_week

--- internal/model/availability.go
+++ internal/model/availability.go
@@ -1,7 +1,7 @@
 package model
 
 type CreateAvailabilityRequest struct {
-	DayOfWeek       int32  `json:"day_of_week" validate:"required" `
+	DayOfWeek       int32  `json:"day_of_week" default:"0" `
 	StartTime       string `json:"start_time" validate:"required" `
 	EndTime         string `json:"end_time"  validate:"required"`
 	IntervalMinutes int32  `json:"interval_minutes"`
