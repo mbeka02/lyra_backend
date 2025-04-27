319148605e7eb46f43e22069264429d110387bdc
mbeka02 <antonymbeka@gmail.com>
2025-04-27 17:05:16 +0300

    added a method for creating or updating a fhir Patient resource

--- internal/fhir/client.go
+++ internal/fhir/client.go
@@ -1,8 +1,11 @@
 package fhir
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	samplyFhir "github.com/samply/golang-fhir-models/fhir-models/fhir"
 	healthcare "google.golang.org/api/healthcare/v1"
@@ -37,5 +40,40 @@ func NewFHIRClient(ctx context.Context, config FHIRConfig, opts ...option.Client
 	return &FHIRClient{svc: svc, basePath: base}, nil
 }
 
-func (f *FHIRClient) UpsertPatient(ctx context.Context, patient *samplyFhir.Patient) {
+func (f *FHIRClient) UpsertPatient(ctx context.Context, patient *samplyFhir.Patient) (*samplyFhir.Patient, error) {
+	payload, err := json.Marshal(patient)
+	if err != nil {
+		return nil, fmt.Errorf("marshal Patient: %w", err)
+	}
+	// hb := &healthcare.HttpBody{Data: string(payload), ContentType: "application/fhir+json"}
+	// var resp *healthcare.HttpBody
+	path := f.basePath + "/Patient/" + *patient.Id
+	var resp *http.Response
+	// If the version Id is emtpy create otherwise update
+	if patient.Meta == nil || patient.Meta.VersionId == nil || *patient.Meta.VersionId == "" {
+		call := f.svc.Projects.Locations.Datasets.FhirStores.Fhir.Create(f.basePath+"/Patient", "Patient", bytes.NewReader(payload))
+		call.Header().Set("Content-Type", "application/fhir+json;charset=utf-8")
+		resp, err = call.Do()
+		if err != nil {
+			return nil, fmt.Errorf("Create resource error: %w", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode > 299 {
+			return nil, fmt.Errorf("Create resource error: status %d : %s", resp.StatusCode, resp.Status)
+		}
+	} else {
+		// Update with PUT; include If-Match header for optimistic concurrency
+		call := f.svc.Projects.Locations.Datasets.FhirStores.Fhir.
+			Update(path, bytes.NewReader(payload))
+		call.Header().Set("Content-Type", "application/fhir+json;charset=utf-8")
+		call.Header().Set("If-Match", fmt.Sprintf(`W/"%s"`, *patient.Meta.VersionId))
+		resp, err = call.Do()
+	}
+	var p samplyFhir.Patient
+	err = json.NewDecoder(resp.Body).Decode(&p)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding patient response: %w", err)
+	}
+	return &p, nil
 }
