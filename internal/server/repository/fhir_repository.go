9f656fcf83373ac38e5f707be624369afa708031
mbeka02 <antonymbeka@gmail.com>
2025-04-28 08:25:05 +0300

    updated fhir patient builder function

--- internal/server/repository/fhir_repository.go
+++ internal/server/repository/fhir_repository.go
@@ -10,28 +10,93 @@ import (
 
 func buildFHIRPatientFromDB(p *database.Patient) *samplyFhir.Patient {
 	patientID := fmt.Sprintf("%d", p.PatientID)
-	return &samplyFhir.Patient{
+
+	// Create enum values as pointers
+	homeAddressUse := samplyFhir.AddressUseHome
+	phoneSystem := samplyFhir.ContactPointSystemPhone
+	homeUse := samplyFhir.ContactPointUseHome
+	seealsoLinkType := samplyFhir.LinkTypeSeealso
+
+	// Create base patient with ID and meta information
+	patient := &samplyFhir.Patient{
 		Id: &patientID,
 		Meta: &samplyFhir.Meta{
 			Profile: []string{"http://hl7.org/fhir/StructureDefinition/Patient"},
+			// LastUpdated: &p.UpdatedAt,
 		},
-		Address: []samplyFhir.Address{{
-			Text: &p.Address,
-		}},
-		Contact: []samplyFhir.PatientContact{{
-			Name: &samplyFhir.HumanName{
-				Text: &p.EmergencyContactName,
-			},
-		}},
-		Extension: []samplyFhir.Extension{
+		// Address information
+		Address: []samplyFhir.Address{
 			{
-				Url:         "http://lyra.com/fhir/StructureDefinition/allergies",
-				ValueString: &p.Allergies,
+				Text: &p.Address,
+				Use:  &homeAddressUse,
 			},
+		},
+		// Emergency contact information
+		Contact: []samplyFhir.PatientContact{
 			{
-				Url:         "http://lyra.com/fhir/StructureDefinition/currentMedication",
-				ValueString: &p.CurrentMedication,
+				Name: &samplyFhir.HumanName{
+					Text: &p.EmergencyContactName,
+				},
+				Telecom: []samplyFhir.ContactPoint{
+					{
+						System: &phoneSystem,
+						Value:  &p.EmergencyContactPhone,
+						Use:    &homeUse,
+					},
+				},
+				Relationship: []samplyFhir.CodeableConcept{
+					{
+						Text: stringPtr("Emergency Contact"),
+					},
+				},
 			},
 		},
 	}
+
+	// Add extension elements for medical and insurance information
+	patient.Extension = []samplyFhir.Extension{
+		{
+			Url:         "http://lyra.com/fhir/StructureDefinition/allergies",
+			ValueString: &p.Allergies,
+		},
+		{
+			Url:         "http://lyra.com/fhir/StructureDefinition/currentMedication",
+			ValueString: &p.CurrentMedication,
+		},
+		{
+			Url:         "http://lyra.com/fhir/StructureDefinition/pastMedicalHistory",
+			ValueString: &p.PastMedicalHistory,
+		},
+		{
+			Url:         "http://lyra.com/fhir/StructureDefinition/familyMedicalHistory",
+			ValueString: &p.FamilyMedicalHistory,
+		},
+	}
+
+	// Add insurance information as coverage reference
+	if p.InsuranceProvider != "" || p.InsurancePolicyNumber != "" {
+		insuranceText := fmt.Sprintf("Provider: %s, Policy: %s", p.InsuranceProvider, p.InsurancePolicyNumber)
+		patient.Extension = append(patient.Extension, samplyFhir.Extension{
+			Url:         "http://lyra.com/fhir/StructureDefinition/insuranceInformation",
+			ValueString: &insuranceText,
+		})
+	}
+
+	// Add link to user account
+	userReference := fmt.Sprintf("User/%d", p.UserID)
+	patient.Link = []samplyFhir.PatientLink{
+		{
+			Other: samplyFhir.Reference{
+				Reference: &userReference,
+			},
+			Type: seealsoLinkType,
+		},
+	}
+
+	return patient
+}
+
+// Helper function to create string pointers
+func stringPtr(s string) *string {
+	return &s
 }
