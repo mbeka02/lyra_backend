064739f173a23135523d6bec591b6f019b161690
mbeka02 <antonymbeka@gmail.com>
2025-05-24 11:29:35 +0300

    updated queries

--- sql/queries/patients.sql
+++ sql/queries/patients.sql
@@ -2,6 +2,7 @@
 INSERT INTO patients(user_id,allergies,current_medication,past_medical_history,family_medical_history,insurance_provider,insurance_policy_number, address,emergency_contact_name , emergency_contact_phone) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)RETURNING *;
 -- name: GetPatientIdByUserId :one
 SELECT patient_id FROM patients WHERE user_id=$1;
-
+-- name: GetPatientById :one
+SELECT * FROM patients WHERE patient_id=$1;
 -- name: UpdateFhirVersionId :exec
 UPDATE  patients SET fhir_version=$1 WHERE patient_id=$2; 
